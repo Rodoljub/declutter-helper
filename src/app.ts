6f0c40f93b91ec529b5fbdcebc1a7a704061f29f
Rodoljub Tanasic <rodoljub.tanasic@outlook.com>
2025-08-15 14:26:22 +0200

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 // app.ts
 import express from "express";
+import cors from "cors";
 import authRoutes from "./features/auth/authRoutes";
 import itemRoutes from "./features/items/itemsRoutes";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:4200", // your Angular dev server
+  credentials: true, // optional, if you use cookies
+}));
+
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
